a7754691eada3a3638a66939d6cdfb6829b4a18c
Juliloio <67295501+Juliloio@users.noreply.github.com>
2020-09-10 14:34:44 -0300

    Update 1_fibonacci.asm

--- 1_fibonacci.asm
+++ 1_fibonacci.asm
@@ -1,11 +1,49 @@
 .data
-fibonacci: .asciiz "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765\n"
+fibonacci: .asciiz "\n"
 
 .text
 .globl main
 main:
-    li  $v0, 4
-    la  $a0, fibonacci
+    li  $t0, 1
+    li  $t1, 1
+   
+    li  $v0, 1
+    move $a0, $t0
 syscall    
+
+   li $v0, 4
+   la $a0, fibonacci
+syscall
+
+    li  $v0, 1
+    move $a0, $t1
+syscall   
+
+   li $v0, 4
+   la $a0, fibonacci
+syscall
+
+li $t3, 18
+
+loop:
+beq $t3, $zero, exit
+add $t2, $t0, $t1
+
+li  $v0, 1
+move $a0, $t2
+syscall   
+
+move $t0, $t1
+move $t1, $t2
+
+li $v0, 4
+la $a0, fibonacci
+syscall
+
+addi $t3, $t3, -1
+
+j loop
+
+exit:
 li   $v0, 10
 syscall
